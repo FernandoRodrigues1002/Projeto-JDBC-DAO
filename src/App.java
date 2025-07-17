2e6e37a13013d5b09b7b52b0658352b5cb9b1d58
Nando <fernanrods1002@gmail.com>
2025-07-17 15:49:53 -0300

    Adicionando seller

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(1, "nando", "nando@mail", new Date(), 300.00, obj);
 
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
