8951f42c705efe51a67af63bf014edb3c84da4e7
Nando <fernanrods1002@gmail.com>
2025-07-17 16:57:00 -0300

    SellerDao e DaoFactory

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "books");
         Seller seller = new Seller(1, "nando", "nando@mail", new Date(), 300.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println(seller);
